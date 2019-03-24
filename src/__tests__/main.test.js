ece26c8b59d7ea3a315c4d3627f356d9efb5be32
Kaspars Dambis <hi@kaspars.net>
2019-03-24 11:11:33 +0200

    We have a lift-off!

---
+++ src/__tests__/main.test.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import changelogBuilder from '../main.js'
+
+jest.mock('axios');
+
+const apiResponseMock = [
+  {
+    "draft": false,
+    "prerelease": false,
+    "name": "Version 1.6.0",
+    "created_at": "2019-03-19T14:50:49Z",
+    "published_at": "2019-03-19T14:52:31Z",
+    "body": "- One log.\r\n\r\n- Mark as tested with WordPress 5.1."
+  },
+  {
+    "draft": false,
+    "prerelease": false,
+    "name": "Version 0.3.0",
+    "created_at": "2018-11-06T08:39:18Z",
+    "published_at": "2018-11-06T08:40:14Z",
+    "body": "- Mark as tested with WordPress 5.0.\r\n\r\n- Always post the two-factor login."
+  },
+  {
+    "draft": true,
+    "prerelease": true,
+    "name": "Version 0.2.0",
+    "created_at": "2018-10-16T07:23:28Z",
+    "published_at": "2018-10-16T07:27:19Z",
+    "body": "- Add developer tools for deploying to WP.org manually."
+  }
+];
+
+describe('buildChangelog', () => {
+  axios.get = jest.fn().mockResolvedValue(apiResponseMock);
+
+  it('can extract and format changelog', async () => {
+    const changelog = await changelogBuilder('dummy/repo')
+
+    expect(changelog).toBe('## Version 1.6.0 (2019-2-19)\n\n- One log.\r\n\r\n- Mark as tested with WordPress 5.1.\n\n## Version 0.3.0 (2018-10-6)\n\n- Mark as tested with WordPress 5.0.\r\n\r\n- Always post the two-factor login.')
+  })
+
+  it('can use custom formatter', async () => {
+    const formatter = (entry) => {
+      return entry.name;
+    }
+
+    const changelog = await changelogBuilder('dummy/repo', formatter)
+
+    expect(changelog).toBe('Version 1.6.0\n\nVersion 0.3.0')
+  })
+})
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { get } from 'axios';
+
+export function buildApiUrl(repoSlug) {
+  return `https://api.github.com/repos/${repoSlug}/releases`
+}
+
+export function formatRelease(release) {
+  const releaseDate = new Date(release.published_at)
+
+  const releaseDateFormatted = [
+    releaseDate.getFullYear(),
+    releaseDate.getMonth(),
+    releaseDate.getDate()
+  ].join('-')
+
+  return `## ${release.name} (${releaseDateFormatted})\n\n${release.body}`
+}
+
+export function formatReleases(releases, formatter) {
+  return releases.filter(release => !release.prerelease && !release.draft).map(formatter).join('\n\n')
+}
+
+export default function releaselog(repoSlug, formatter = formatRelease) {
+  const apiUrl = buildApiUrl(repoSlug)
+
+  return get(apiUrl).then(response => formatReleases(response, formatter))
+}
